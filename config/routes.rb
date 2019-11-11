dfc428ac17608b37658c68bb959df6c9074e741a
kento.uoi <fishperson19@gmail.com>
2019-11-11 21:34:02 +0900

    route

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
  root 'messages#index'
- resources :users, only: [:edit, :update]
+ resources :users, only: [:index, :edit, :update]
+ resources :groups, only: [:new, :create, :edit, :update]
 end
