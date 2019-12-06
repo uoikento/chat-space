fe2d964695e883ac7919198299800216755a1fbc
kento.uoi <fishperson19@gmail.com>
2019-12-06 19:46:45 +0900

    correct-capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid for current version and patch releases of Capistrano
-lock " 3.11.2"
+lock "3.11.2"
 
 set :application, "chat-space"
 set :repo_url, "git@github.com:uoikento/chat-space.git"
@@ -7,9 +7,13 @@ set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', '
 set :rbenv_type, :user
 set :rbenv_ruby, '2.5.1'
 set :ssh_options, auth_methods: ['piblickey'], keys: ['~/.ssh/f-chatspace.pem']
-set :unicorn_pid, -> {"#{shared_path}/tmp/pids/unicorn.pid"}
-set :unicorn_config_path, -> {"#{current_path}/config/unicorn.rb"}
+set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
+
+# Unicornの設定ファイルの場所
+set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
+
+# デプロイ処理が終わった後、Unicornを再起動するための記述
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
   task :restart do
